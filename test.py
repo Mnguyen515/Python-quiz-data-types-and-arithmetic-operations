37f11fbed64d4d098152ed47a41c1c5e3c7841e4
Michael Nguyen <michael.nguyen.21@us.af.mil>
2024-10-09 10:15:21 -0500

    First Commit

---
+++ test.py
@@ -0,0 +1,19 @@
+employees = {
+    "E001": {
+        "name": "Fiona",
+        "age": 29,
+        "department": "HR"
+    },
+    "E002": {
+        "name": "George",
+        "age": 34,
+        "department": "Engineering"
+    }
+}
+
+# Print the department of employee E002
+print(employees["E002"]['department'])  # Expected Output: "Engineering"
+
+
+for emp_id, details in employees.items():
+    print(f"ID: {emp_id}, Name: {emp_id}, Department: {emp_id}")
